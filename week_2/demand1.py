cb00b25aeb37902159d6068fbabca4130601f9fd
許配瑜 <peiui0807@gmail.com>
2021-03-16 20:33:37 +0800

    Update demand1.py

--- week_2/demand1.py
+++ week_2/demand1.py
@@ -4,6 +4,7 @@ def calculate(min,max):
     for i in range(min, max+1):
         count=count+i
     print(count)
+    
 calculate(1,3)
 calculate(4,8)
 
@@ -45,7 +46,25 @@ def twoSum(nums,target):
     for i in range(len(nums)):
         for j in range(i+1,len(nums)):
             if(nums[i]+nums[j]==target):
-                result = [i,j]
+                result=[i,j]
     print(result)
 
 result=twoSum([2, 11, 7, 15], 9)
+
+#第五題
+def maxZeros(nums):
+    count=0
+    maxcount=0
+    for i in range(len(nums)):
+        if(nums[i]==0):
+            count=count+1
+        else:
+            count=0
+        if(maxcount<count):
+            maxcount=count
+    print(maxcount)
+
+maxZeros([0, 1, 0, 0])
+maxZeros([1, 0, 0, 0, 0, 1, 0, 1, 0, 0])
+maxZeros([1, 1, 1, 1, 1])
+
