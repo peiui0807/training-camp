874345db004f6808338dd8acd1a283f8f2d857f3
PEI HSU <peiui0807@gmail.com>
2021-04-05 10:10:27 +0800

    Update hw6.py

--- week_6/hw6.py
+++ week_6/hw6.py
@@ -8,7 +8,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="fish8888",
+    password="",
     database="mydatabase"
 )
 mycursor = mydb.cursor()
